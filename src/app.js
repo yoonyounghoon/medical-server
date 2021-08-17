10ea4096e9d1d4260ba1ed978685fff575bae72d
yoonyounghoon <yunyounghoon23@gmail.com>
2021-08-17 11:54:44 +0900

    Implements 병원 하나에 대한 자세한 정보 API

--- src/app.js
+++ src/app.js
@@ -12,9 +12,12 @@ function RemoveJsonTextAttribute(value, parentElement) {
     var keyNo = Object.keys(parentElement._parent).length;
     var keyName = Object.keys(parentElement._parent)[keyNo - 1];
     parentElement._parent[keyName] = value;
-  } catch (e) {}
+  } catch (e) {
+    console.log(e);
+  }
 }
 
+// 시도, 시군구 기준으로 병원 가져오기
 app.get('/hospital/:siDoCd/:siGunGuCd', async (req, res) => {
   let siDoCd = req.params.siDoCd;
   let siGunGuCd = req.params.siGunGuCd;
@@ -26,14 +29,39 @@ app.get('/hospital/:siDoCd/:siGunGuCd', async (req, res) => {
       compact: true,
       spaces: 4,
       ignoreDoctype: true,
+      textFn: RemoveJsonTextAttribute,
     });
-    res.json(response.response.body.items);
+    res.json(response.response.body.items.item);
   } catch (e) {
-    console.log(e);
-    res.send('error!');
+    res.status(500).send({ message: e });
   }
 });
 
+// 병원 하나에 대한 자세한 정보 API
+app.get('/hospital/:name', async (req, res) => {
+  let hospitalName = req.params.name;
+  // 문자열 공백제거
+  hospitalName = hospitalName.replace(/ /gi, '');
+  console.log(hospitalName);
+  // 인코딩
+  hospitalName = encodeURI(hospitalName);
+  let url = `http://apis.data.go.kr/B552657/HsptlAsembySearchService/getHsptlMdcncListInfoInqire?serviceKey=${process.env.API_KEY}&QN=${hospitalName}`;
+  try {
+    let response = await request(url);
+
+    response = convert.xml2js(response, {
+      compact: true,
+      spaces: 4,
+      ignoreDoctype: true,
+      textFn: RemoveJsonTextAttribute,
+    });
+    res.json(response.response.body.items.item);
+  } catch (e) {
+    res.status(500).send({ message: e });
+  }
+});
+
+// 좌표 기준으로 영업중인 약국 가져오는 API
 app.get('/pharmacy/:yPos/:xPos', async (req, res) => {
   let xPos = req.params.xPos;
   let yPos = req.params.yPos;
@@ -49,11 +77,11 @@ app.get('/pharmacy/:yPos/:xPos', async (req, res) => {
     });
     res.json(response.response.body.items.item);
   } catch (e) {
-    console.log(e);
-    res.send('error!');
+    res.status(500).send({ message: e });
   }
 });
 
+// 약국 하나에 대한 디테일 정보 ( 영업시간 )
 app.get('/pharmacy/:id', async (req, res) => {
   let id = req.params.id;
   let url = `http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyBassInfoInqire?serviceKey=OW%2B7pApxaWyA%2FMtp4h809DV8bN%2FTeDY%2B6RVsTr5ZsS4Peuz2gAmUOTBpsqyZc0ITRd%2FIyE5qor%2B2cCxo42moPQ%3D%3D&HPID=${id}&pageNo=1&numOfRows=10`;
@@ -68,8 +96,7 @@ app.get('/pharmacy/:id', async (req, res) => {
     });
     res.json(response.response.body.items.item);
   } catch (e) {
-    console.log(e);
-    res.send('error!');
+    res.status(500).send({ message: e });
   }
 });
 
